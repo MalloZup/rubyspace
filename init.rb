a9d474bf2d6087cc1ee9f47152b2fa5c736f5e0d
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-25 15:08:30 +0100

    Refractoring: use 1line for

--- init.rb
+++ init.rb
@@ -1,17 +1,21 @@
 #! /usr/bin/ruby
 require 'xmlrpc/client'
 
-@TESTHOST = "TEST"
+TESTHOST = "test"
 @SATELLITE_URL = "http://#{TESTHOST}/rpc/api"
 @SATELLITE_LOGIN = "admin"
 @SATELLITE_PASSWORD = "admin"
 
 @client = XMLRPC::Client.new2(@SATELLITE_URL)
-
 @key = @client.call('auth.login', @SATELLITE_LOGIN, @SATELLITE_PASSWORD)
-channels = @client.call('channel.listAllChannels', @key)
-for channel in channels do
-   p channel["label"]
+
+# Print and return List all channels
+def listAllChannels(key)
+  channels = @client.call('channel.listAllChannels', key)
+  channels.each { |ch| puts ch["label"] }
 end
 
+
+
+listAllChannels(@key)
 @client.call('auth.logout', @key)
