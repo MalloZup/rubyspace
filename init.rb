886e5f4112722a9b6546a4eae298f1e39d21114e
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-25 16:53:41 +0100

    Some tests.

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/ruby
 require 'xmlrpc/client'
 
-TESTHOST = "test"
+TESTHOST = "headref-suma3pg.mgr.suse.de/"
 @SATELLITE_URL = "http://#{TESTHOST}/rpc/api"
 @SATELLITE_LOGIN = "admin"
 @SATELLITE_PASSWORD = "admin"
@@ -10,12 +10,28 @@ TESTHOST = "test"
 @key = @client.call('auth.login', @SATELLITE_LOGIN, @SATELLITE_PASSWORD)
 
 # Print and return List all channels
-def listAllChannels(key)
-  channels = @client.call('channel.listAllChannels', key)
+def listAllChannels()
+  channels = @client.call('channel.listAllChannels', @key)
   channels.each { |ch| puts ch["label"] }
+end
+
+# List and return all pkg from a channel label given.
+def listAllPackages(channelLabel)
+  puts "list packages in channel"
+  @client.call('channel.software.listAllPackages', @key,  channelLabel)
+end
+
+# Sync a repo 
+def SyncRepo(channelLabel)
+  @client.call('channel.software.syncRepo', @key,  channelLabel)
 end
 
 
 
-listAllChannels(@key)
+### Main ## 
+listAllChannels()
+# SyncRepo("sles12-sp1-pool-x86_64")
+# listAllPackages("sles12-sp1-pool-x86_64")
+puts @client.call('schedule.listInProgressActions', @key)
+puts @client.call('schedule.listFailedActions', @key)
 @client.call('auth.logout', @key)
