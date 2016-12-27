7570dc27301aa76048cd677272ba6220e824a80d
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-27 21:18:18 +0100

    Implement user.rb with class inheritance

--- test/random_creation.rb
+++ test/random_creation.rb
@@ -9,28 +9,31 @@ MINION = 'head-minsles12sp1.tf.local'.freeze
 @space_login = 'admin'
 @space_pwd = 'admin'
 
-@client = XMLRPC::Client.new2(@space_url)
-@key = @client.call('auth.login', @space_login, @space_pwd)
+$client = XMLRPC::Client.new2(@space_url)
+$key = $client.call('auth.login', @space_login, @space_pwd)
 
-def random_create_users(user_number)
-  user_number.times do
-    pwd = SecureRandom.urlsafe_base64(10)
-    f_name = SecureRandom.urlsafe_base64(5)
-    l_name = SecureRandom.urlsafe_base64(6)
-    mail = SecureRandom.urlsafe_base64(4) + '@' + SecureRandom.urlsafe_base64(3)
-    @client.call('user.create', @key, pwd, pwd, f_name, l_name, "#{mail}.com")
+class Random
+
+  def self.create_users(user_number)
+    user_number.times do
+      pwd = SecureRandom.urlsafe_base64(10)
+      f_name = SecureRandom.urlsafe_base64(5)
+      l_name = SecureRandom.urlsafe_base64(6)
+      mail = SecureRandom.urlsafe_base64(4) + '@' + SecureRandom.urlsafe_base64(3)
+      $client.call('user.create', $key, pwd, pwd, f_name, l_name, "#{mail}.com")
+    end
   end
-end
 
-def random_create_sysgroup(group_number)
-  group_number.times do
+  def self.create_sysgroup(group_number)
+    group_number.times do
     desc = SecureRandom.urlsafe_base64(9)
     name = SecureRandom.urlsafe_base64(7)
-    @client.call('systemgroup.create', @key, name, desc)
+    $client.call('systemgroup.create', $key, name, desc)
   end
-end
+  end
+end 
 
 number = 1000
 # random_create_users(10000)
-random_create_sysgroup(number)
-@client.call('auth.logout', @key)
+Random.create_sysgroup(number)
+$client.call('auth.logout', @key)
--- test/user.rb
+++ test/user.rb
@@ -5,21 +5,39 @@ require 'securerandom'
 
 TESTHOST = 'rfsuma3pg.mgr.suse.de'.freeze
 MINION = 'head-minsles12sp1.tf.local'.freeze
-@space_url = "http://#{TESTHOST}/rpc/api"
-@space_login = 'admin'
-@space_pwd = 'admin'
+$space_url = "http://#{TESTHOST}/rpc/api"
+$space_login = 'admin'
+$space_pwd = 'admin'
 
-@client = XMLRPC::Client.new2(@space_url)
-@key = @client.call('auth.login', @space_login, @space_pwd)
+class Auth
 
-def cr_user_readonly
-  @client.call('user.create', @key, 'readonly', 'readonly',
-               'readonly', 'Verde', 'pino@foo.com')
-rescue XMLRPC::FaultException => detail
-  puts 'Got an exception :'
-  puts detail
-  @client.call('user.setReadOnly', @key, 'readonly', true)
-  @client.call('auth.logout', @key)
+ def initialize(space_url, space_login, space_pwd)
+   @space_url = space_url
+   @space_login = space_login
+   @space_pwd = space_pwd
+   @@client = nil
+   @@key = nil
+ end
+ 
+ def self.gen_key(space_url, space_login, space_pwd)
+    @@client = XMLRPC::Client.new2(space_url)
+    @@key = @@client.call('auth.login', space_login, space_pwd)
+ end
 end
 
-@client.call('auth.logout', @key)
+class User < Auth
+  def self.cr_user_readonly
+    @@client.call('user.create', @@key, 'readonly', 'readonly',
+                 'readonly', 'Verde', 'pino@foo.com')
+  rescue XMLRPC::FaultException => detail
+    puts 'Got an exception :'
+    puts detail
+    @@client.call('user.setReadOnly', @@key, 'readonly', true)
+    @@client.call('auth.logout', @@key)
+  end
+end
+
+Auth.gen_key($space_url, $space_login, $space_pwd)
+User.cr_user_readonly
+
+# $client.call('auth.logout', $key)
