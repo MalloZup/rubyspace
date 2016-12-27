871804b37a94bcb00af2d0dfeeba7ef0dfb61ade
Dario Maiocchi <dmaiocchi@suse.com>
2016-12-27 14:44:34 +0100

    Add function to create X random users

--- user.rb
+++ user.rb
@@ -1,14 +1,16 @@
 #! /usr/bin/ruby
 
 require 'xmlrpc/client'
+require 'securerandom'
 
-TESTHOST = 'foo'.freeze
+TESTHOST = 'head-suma3pg.mgr.suse.de'.freeze
 MINION = 'head-minsles12sp1.tf.local'.freeze
 @space_url = "http://#{TESTHOST}/rpc/api"
 @space_login = 'admin'
 @space_pwd = 'admin'
 
 @client = XMLRPC::Client.new2(@space_url)
+@key = @client.call('auth.login', @space_login, @space_pwd)
 
 def cr_user_readonly
   @client.call('user.create', @key, 'readonly', 'readonly',
@@ -19,3 +21,16 @@ rescue XMLRPC::FaultException => detail
   @client.call('user.setReadOnly', @key, 'readonly', true)
   @client.call('auth.logout', @key)
 end
+
+def create_users(user_number)
+  user_number.times do
+    pwd = SecureRandom.urlsafe_base64(10)
+    f_name = SecureRandom.urlsafe_base64(5)
+    l_name = SecureRandom.urlsafe_base64(6)
+    mail = SecureRandom.urlsafe_base64(4) + '@' + SecureRandom.urlsafe_base64(3)
+    @client.call('user.create', @key, pwd, pwd, f_name, l_name, "#{mail}.com")
+  end
+end
+
+# create_users(100)
+@key = @client.call('auth.logout', @space_login, @space_pwd)
